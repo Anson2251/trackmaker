627aa3ea646265423c5fcc1a4dd2307b29f9e618
Heyan Zhu <zhu_heyan@163.com>
2024-04-18 08:28:47 +0800

    remove the protection of credentials

--- src/App.vue
+++ src/App.vue
@@ -45,14 +45,4 @@ nav a:first-of-type {
 	border: 0;
 }
 
-@media (min-width: 1024px) {
-	nav {
-		text-align: left;
-		margin-left: -1rem;
-		font-size: 1rem;
-
-		padding: 1rem 0;
-		margin-top: 1rem;
-	}
-}
 </style>
