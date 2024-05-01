d6f0c67086960eacdcfb858096f9a25922c5daac
Heyan Zhu <zhu_heyan@163.com>
2024-05-01 10:56:57 +0800

    improve startUpdatingService compatibility with Chrome

--- src/script/geoLocation.ts
+++ src/script/geoLocation.ts
@@ -1,4 +1,4 @@
-import  * as prcoords  from "prcoords/js/PRCoords.js";
+import * as prcoords from "prcoords/js/PRCoords.js";
 
 export interface Location {
     latitude: number,
@@ -9,32 +9,32 @@ const geoLocationPresent: Location = {
     latitude: 0,
     longitude: 0
 };
-(window as any).geoLocationPresent = geoLocationPresent
 
-let onchangeCallback: {id: number, callback: (location: Location) => void}[] = [];
-function doCallbacks(){
+let onchangeCallback: { id: number, callback: (location: Location) => void }[] = [];
+function doCallbacks() {
     onchangeCallback.forEach((item) => item.callback(geoLocationPresent));
 }
 
 export let updateServiceStarted = false;
 export let updateServiceUpdating = true;
 
-export function getGeoLocationPresent(){
+export function getGeoLocationPresent() {
     return geoLocationPresent;
 }
 
-export function isUpdateServiceExist(){
+export function isUpdateServiceExist() {
     return updateServiceStarted;
 }
 
-export function isUpdateServiceUpdating(){
+export function isUpdateServiceUpdating() {
     return updateServiceUpdating;
 }
 
-export function startUpdatingService(){
-    if(updateServiceStarted) return;
+export function startUpdatingService() {
+    if (updateServiceStarted) return;
     updateServiceUpdating = true;
-    return navigator.geolocation.watchPosition((position) =>  {
+
+    const successCallback = (position: GeolocationPosition) => {
         const newPosition = convertCoordinates({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
@@ -43,20 +43,29 @@ export function startUpdatingService(){
         geoLocationPresent.longitude = newPosition.longitude
         updateServiceStarted = true;
         updateServiceUpdating = false;
+        console.log(newPosition)
         doCallbacks();
-    }, (error) => {
-        console.log("Error: " + error.message);
+    }
+
+    const errorcallback = (error: GeolocationPositionError) => {
+        console.error("Error: " + error.message);
         updateServiceUpdating = false;
-    });
+    }
+
+    const options: PositionOptions = { enableHighAccuracy: true }
+
+    // for the initial value
+    navigator.geolocation.getCurrentPosition(successCallback, errorcallback, options);
+    return navigator.geolocation.watchPosition(successCallback, errorcallback, options);
 }
 
-export function stopUpdatingService(id: number){
+export function stopUpdatingService(id: number) {
     navigator.geolocation.clearWatch(id);
     updateServiceStarted = false;
 }
 
 export function convertCoordinates(location: Location): Location {
-    const converted = prcoords.wgs_gcj({lat: location.latitude, lon: location.longitude}, true)
+    const converted = prcoords.wgs_gcj({ lat: location.latitude, lon: location.longitude }, true)
     return { latitude: converted.lat, longitude: converted.lon }
 }
 
@@ -66,7 +75,7 @@ export function supportGeolocation(): boolean {
 
 export function addChangeListener(callback: (location: Location) => void) {
     const id = onchangeCallback.length > 0 ? onchangeCallback[onchangeCallback.length - 1].id + 1 : 0;
-    onchangeCallback.push({id: id, callback: callback});
+    onchangeCallback.push({ id: id, callback: callback });
 }
 
 export function removeChangeListener(id: number) {
