62473429c699181251e772be5cc4b18511714cf2
Anson2251 <78967291+Anson2251@users.noreply.github.com>
2024-05-06 04:26:43 +0000

    Deploying to gh-pages-demo from @ Anson2251/trackmaker@18f2fd706ecebb7edd28a14eb0fb0d06e723ab90 🚀

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.vue' {
-    import { DefineComponent } from 'vue';
-
-    const componentOptions: DefineComponent<{}, {}, any>;
-    export default componentOptions;
-}
