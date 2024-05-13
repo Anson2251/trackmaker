d12af9d40e5ddd6d2b48396da1f54370bba57c25
Heyan Zhu <zhu_heyan@163.com>
2024-05-13 20:59:34 +0800

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-console.log(process.env.BING_MAPS_KEY)
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	define: {
