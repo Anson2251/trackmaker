88945bd0bb5b78b60d6315645dc578b91e29fc94
Heyan Zhu <zhu_heyan@163.com>
2025-02-10 23:31:05 +0800

    initial implement for the controls of terra draw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,159 @@
 <script lang="ts">
 	import "../app.css";
-	import { MapLibre, ControlGroup, ControlButton } from "svelte-maplibre";
+	import {
+		MapLibre,
+		ControlGroup,
+		Control,
+		ControlButton,
+		NavigationControl,
+		GeolocateControl,
+		FullscreenControl,
+		ScaleControl,
+	} from "svelte-maplibre";
 	import { TerraDrawMapLibreGLAdapter } from "terra-draw-maplibre-gl-adapter";
-	import { TerraDraw, TerraDrawRectangleMode } from "terra-draw";
+	import {
+		TerraDraw,
+		TerraDrawRectangleMode,
+		TerraDrawPointMode,
+		TerraDrawSelectMode,
+		TerraDrawPolygonMode,
+		TerraDrawLineStringMode,
+		TerraDrawCircleMode,
+		TerraDrawFreehandMode,
+	} from "terra-draw";
 	import { Map } from "maplibre-gl";
 	import { invoke } from "@tauri-apps/api/core";
 	import { Card } from "flowbite-svelte";
+	import {
+		IconMapPin,
+		IconVector,
+		IconCircle,
+		IconLine,
+		IconPolygon,
+		IconHandFinger,
+	} from "@tabler/icons-svelte";
+	import {
+		injectCustomGeolocationProvider,
+		IPGeolocation,
+		locateTauri,
+	} from "../lib/geolocation";
+
+	injectCustomGeolocationProvider(IPGeolocation);
+	locateTauri()
+		.then((coordinate) => {
+			location.latitude = coordinate.latitude;
+			location.longitude = coordinate.longitude;
+		})
+		.catch((error) => {
+			console.error("Error locating via Tauri API:", error);
+		});
 
 	let location = $state<{ latitude: number; longitude: number }>({
 		latitude: 50,
 		longitude: 20,
 	});
 
-	async function getGeolocation() {
-		location = await invoke("get_geolocation");
-		if (location == null) {
-			console.error(
-				"Fail to get the location via https://freeipapi.com/api/json",
-			);
-		}
-	}
+	type drawerModesItem = {
+		mode: any;
+		name: string;
+		icon: any;
+	};
 
-	function initPlugins(map: Map) {
-		const draw = new TerraDraw({
-			adapter: new TerraDrawMapLibreGLAdapter({ map }),
-			modes: [new TerraDrawRectangleMode()],
+	let drawerModes: drawerModesItem[] = [
+		{
+			mode: new TerraDrawRectangleMode(),
+			name: "TerraDraw: Rectangle",
+			icon: IconVector,
+		},
+		{
+			mode: new TerraDrawPointMode(),
+			name: "TerraDraw: Point",
+			icon: IconMapPin,
+		},
+		{
+			mode: new TerraDrawPolygonMode(),
+			name: "TerraDraw: Polygon",
+			icon: IconPolygon,
+		},
+		{
+			mode: new TerraDrawCircleMode(),
+			name: "TerraDraw: Circle",
+			icon: IconCircle,
+		},
+		{
+			mode: new TerraDrawLineStringMode(),
+			name: "TerraDraw: Line String",
+			icon: IconLine,
+		},
+		{
+			mode: new TerraDrawSelectMode(),
+			name: "TerraDraw: Select",
+			icon: IconHandFinger,
+		},
+	];
+
+	let map: Map | null = null;
+	let draw: TerraDraw | null = null;
+
+	function initMap(mapInstance: Map) {
+		map = mapInstance;
+		draw = new TerraDraw({
+			adapter: new TerraDrawMapLibreGLAdapter({ map: mapInstance }),
+			modes: drawerModes.map((item) => item.mode),
 		});
-		draw.start();
-		draw.setMode("rectangle");
 	}
-
-	getGeolocation();
 </script>
 
-<Card padding="none" class="overflow-hidden" style="margin: 2em">
-	<h3 class="text-xl font-bold mb-4">Map with TerraDraw</h3>
-	<MapLibre
-		class="map"
-		onload={initPlugins}
-		center={[location.longitude, location.latitude]}
-		zoom={7}
-		standardControls
-		style={`https://api.maptiler.com/maps/basic-v2/style.json?key=${__MAP_TILER_KEY__}`}
-	/>
-</Card>
+<div class="map-container">
+	<Card
+		padding="none"
+		class="overflow-hidden h-full"
+		style="max-width: unset !important; max-height: unset !important;"
+	>
+		<MapLibre
+			class="map"
+			onload={initMap}
+			center={[location.longitude, location.latitude]}
+			zoom={7}
+			style={`https://api.maptiler.com/maps/basic-v2/style.json?key=${__MAP_TILER_KEY__}`}
+		>
+			{#snippet children()}
+				<NavigationControl />
+				<GeolocateControl />
+				<FullscreenControl />
+				<ScaleControl />
+				<Control class="flex flex-col gap-y-2">
+					<ControlGroup>
+						{#each drawerModes as item}
+							<ControlButton
+								icon={true}
+								center={true}
+								title={item.name}
+								onclick={() => {
+									draw?.start();
+									draw?.setMode(item.mode.mode);
+								}}
+							>
+								<item.icon />
+							</ControlButton>
+						{/each}
+					</ControlGroup>
+				</Control>
+			{/snippet}
+		</MapLibre>
+	</Card>
+</div>
 
 <style>
 	:global(.map) {
-		height: 500px;
+		height: 100%;
+	}
+
+	.map-container {
+		--margin-width: 2em;
+		margin: var(--margin-width);
+
+		width: calc(100% - calc(var(--margin-width) * 2));
+		height: calc(100% - calc(var(--margin-width) * 2));
 	}
 </style>
