1019fb1b466db36076324a5862f04dfd35defefa
Heyan Zhu <zhu_heyan@163.com>
2026-01-13 19:06:32 +0800

    fix(vite): update vite's dev port

--- vite.config.ts
+++ vite.config.ts
@@ -77,7 +77,7 @@ async function generateConfig(): Promise<UserConfig> {
         clearScreen: false,
         // 2. tauri expects a fixed port, fail if that port is not available
         server: {
-            port: 1200,
+            port: 1420,
             strictPort: true,
             host: host || false,
             hmr: host
